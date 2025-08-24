d98918062b68a8c0384e110002abbe7bfa4cbefc
Joydeep Dey <dey.joydeep.cse@gmail.com>
2025-08-24 10:23:09 +0900

    fix(web-ui): export public API (ConfirmDialogComponent, MaterialModule) to satisfy ng-packag

--- libs/web/ui/src/index.ts
+++ libs/web/ui/src/index.ts
@@ -1,3 +1,5 @@
 // Public API for the UI library
 export * from './ui.module';
 export * from './models/date-range.model';
+export * from './components';
+export * from './material/material.module';
